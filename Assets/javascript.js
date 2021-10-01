0713250e4e88d549b977545bc7f580d253bd7790
Gabriel Calk <gabrielcalk@outlook.com>
2021-10-01 17:43:54 -0400

    Get the API and made the autocomplete work

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,1 +1,22 @@
-// https://openweathermap.org/api
+// API URL missing just the name of the city and the country
+const url_api = ('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=6086d1f039acf014abeacd1138429b35&units=imperial&q=')
+
+
+$( function() {
+    var availableTags = ['Orlando', 'Atlanta', 'Denver'];
+    $( "#input_cities" ).autocomplete({
+      source: availableTags,
+    });
+  } );
+
+  console.log('oi')
+
+
+//   fetch(url_api)
+// .then(function (response){
+//     console.log(response)
+//     return response.json();
+// })
+// .then(function (data){
+//     console.log(data)
+// })
