cc9b61171e40fb1e0f76b6626fa96604eba39e37
unknown <gabrielcalk@outlook.com>
2021-10-01 21:48:04 -0400

    Adding a new API, created on event to the button and added the input value to an button below

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,22 +1,37 @@
-// API URL missing just the name of the city and the country
-const url_api = ('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=6086d1f039acf014abeacd1138429b35&units=imperial&q=')
+const input_cities = document.querySelector('#input_cities')
+const button_city = document.querySelector('#button_city')
+const cities_chosen_div = document.querySelector('#cities_chosen_div')
+const cities_chosen_button = document.querySelector('#cities_chosen_button')
 
-
+// Adding the autocomplete on the input
 $( function() {
-    var availableTags = ['Orlando', 'Atlanta', 'Denver'];
+    var availableTags = ['Orlando', 'Atlanta', 'Denver', 'New York', 'Boston', 'Chicago'];
     $( "#input_cities" ).autocomplete({
       source: availableTags,
     });
   } );
 
-  console.log('oi')
+function get_city(){
+// Getting the value from the input and taking out all the space that user could put
+  var city = input_cities.value.trim();
 
+  const url_api = ('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=6086d1f039acf014abeacd1138429b35&units=imperial&q='+city+',EUA')
+// Getting the data from the weather API using the input value
+  fetch(url_api)
+    .then(function (response){
+      return response.json();
+  })
+    .then(function (data){
+      return get_wheater(data);
+  })
+  }
 
-//   fetch(url_api)
-// .then(function (response){
-//     console.log(response)
-//     return response.json();
-// })
-// .then(function (data){
-//     console.log(data)
-// })
+function get_wheater(data){
+  cities_chosen_button.classList.remove('hide');
+  cities_chosen_button.innerHTML = data.city.name;
+  console.log(data.list[0].weather[0]);
+  
+}
+
+// Calling the get_wheater function after click on the button
+button_city.addEventListener('click', get_city);
