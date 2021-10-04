f453decc59b9e4a167d10a95344e13d0ebef4c05
Gabriel Cavalcante <gabrielcalk@outlook.com>
2021-10-03 20:00:05 -0400

    Finalized the js of the page and added some comments

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,3 +1,4 @@
+// Parsing the values that is on the local storage
 const parse_saved_cities = JSON.parse(localStorage.getItem('city'));
 
 const input_cities = document.querySelector('#input_cities')
@@ -11,18 +12,26 @@ const wind = document.querySelector('#wind');
 const humidity = document.querySelector('#humidity');
 const uv_index = document.querySelector('#uv_index');
 
+// Array that contain the city that have been chosen
 const cities_saved = []
 
-get_cities_saved()
+// Ff the value on the local storage is differente than null, then active the get_cities_saved function
+if (parse_saved_cities !== null){
+  get_cities_saved();
+}
 
+// Getting the cities name that was storaged local and putting on the doc in a button
 function get_cities_saved(){
+
+// Creating one button, adding text content and adding data-value on the cities that was on the local storage
   for (i = 0; i < parse_saved_cities.length; i++){
     var button_cities = document.createElement('button');
-    button_cities.classList.add('get');
     button_cities.setAttribute('data-value', parse_saved_cities[i].texts);
     button_cities.textContent = parse_saved_cities[i].texts;
     cities_chosen_div.append(button_cities);
   }
+// If the user clicked on the button that cointain the citie name, then active the get_cities_saved function 
+  cities_chosen_div.addEventListener('click', get_city_chosen);
 }
 
 // Adding the autocomplete on the input
@@ -33,27 +42,37 @@ $( function() {
     });
   } );
 
+// Checking if the input value is not a number or null, if it is, then pop up one alert saying that suppose to be one valid city on EUA
 function get_city(){
+  if (input_cities.value == ''){
+    alert('Please, insert one city on EUA')
+  } else if (!isNaN(input_cities.value)){
+    alert('Insert one city on EUA')
+  } else {
+
 // Getting the value from the input and taking out all the space that user could put
   var city = input_cities.value.trim();
+
+// Getting one button for the city, adding data and appending to the div on the dom
   var button_cities = document.createElement('button');
-  button_cities.classList.add('get')
+  button_cities.setAttribute('data-value', city);
   button_cities.textContent = city;
   cities_chosen_div.append(button_cities);
 
-  button_cities.setAttribute('data-value', city);
-
+// Storing the cities in on object that will be push to an array
   var object_city = {
     texts: city
   }
-
   cities_saved.push(object_city);
+
+// Setting the cities to the local storage
   localStorage.setItem('city', JSON.stringify(cities_saved));
 
+// Cleaning the input after the user clicked to see the weather on one city
   input_cities.value = '';
 
-  const url_api_weather = ('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=6086d1f039acf014abeacd1138429b35&units=imperial&q='+city+',EUA')
 // Getting the data from the weather API using the input value
+  const url_api_weather = ('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=6086d1f039acf014abeacd1138429b35&units=imperial&q='+city+',EUA')
   fetch(url_api_weather)
     .then(function (response){
       return response.json();
@@ -61,55 +80,58 @@ function get_city(){
     .then(function (data){
       return get_wheater(data);
   })
+  }
 }
 
+// Function that will be actived after get the data from the city that the user wrote on the input
 function get_wheater(data){
+// Adding Today's Day
   city_name.innerHTML = data.city.name+' '+moment().format("(MM-DD-YYYY)");  
-  
+
+// Adding the temp, wind, humidity and uv_index about today on the dom
   temp.innerHTML = ' '+data.list[0].main.temp+'ºF'
   wind.innerHTML = ' '+data.list[0].wind.speed+' MPH'
   humidity.innerHTML = ' '+data.list[0].main.humidity+'%'
   uv_index.innerHTML = ' '+'pending'
 
+  // Adding the image that best represents the general weather of the day
   if (data.list[0].weather[0].main == "Clear"){
     img_weather_today.classList.remove('hide')
-    img_weather_today.setAttribute('src', 'Assets/weather-icons/day.svg')
+    img_weather_today.setAttribute('src', 'Assets/weather-icons/sun.png')
     img_weather_today.setAttribute('alt', 'clear wheater')
-    
-  } else if (data.list[0].weather[0].description == "broken clouds"){
-    img_weather_today.classList.remove('hide')
-    img_weather_today.setAttribute('src', 'Assets/weather-icons/cloudy-day-1.svg')
-    img_weather_today.setAttribute('alt', 'broken clouds')
   
   } else if (data.list[0].weather[0].main == "Clouds"){
     img_weather_today.classList.remove('hide')
-    img_weather_today.setAttribute('src', 'Assets/weather-icons/cloudy.svg')
+    img_weather_today.setAttribute('src', 'Assets/weather-icons/cloudy.png')
     img_weather_today.setAttribute('alt', 'clouds')
   
   } else if (data.list[0].weather[0].main == "Rain"){
     img_weather_today.classList.remove('hide')
-    img_weather_today.setAttribute('src', 'Assets/weather-icons/rainy-6.svg')
+    img_weather_today.setAttribute('src', 'Assets/weather-icons/rain.png')
     img_weather_today.setAttribute('alt', 'rain')
   
   } else if (data.list[0].weather[0].main == "Snow"){
     img_weather_today.classList.remove('hide')
-    img_weather_today.setAttribute('src', 'Assets/weather-icons/snowy-5.svg')
+    img_weather_today.setAttribute('src', 'Assets/weather-icons/snowy.png')
     img_weather_today.setAttribute('alt', 'snow')
   
   } else{
     img_weather_today.classList.remove('hide')
-    img_weather_today.setAttribute('src', 'Assets/weather-icons/thunder.svg')
+    img_weather_today.setAttribute('src', 'Assets/weather-icons/extreme.png')
     img_weather_today.setAttribute('alt', 'extreme')
   }
+
+// After adding the Image, then pass the data from the api to another function to add the next 5 days weather
   get_wheater_5(data)
 }
 
 function get_wheater_5(data){
-  console.log(data)
+// Doing a loop to add the temp, wind, humidity and one image to the next 5 days weather
   for (i = 1; i <= 5; i++){
       var time_5 = document.querySelector('#time'+[i]);
       time_5.textContent = moment().add([i],'days').format("MM/DD/YYYY");
-      
+
+// Selecting the temp, wind, humidity and image from the html file
       var temp_5 = document.querySelector('#temp'+[i]);
       var wind_5 = document.querySelector('#wind'+[i]);
       var humidity_5 = document.querySelector('#humidity'+[i]) ;
@@ -119,49 +141,47 @@ function get_wheater_5(data){
       wind_5.textContent = ` ${data.list[i*7].wind.speed} MPH`;
       humidity_5.textContent = ` ${data.list[i*7].main.humidity} %`;
 
+// Adding the image that best represents the general weather of the day
       if (data.list[i*7].weather[0].main == "Clear"){
         img_5.classList.remove('hide')
-        img_5.setAttribute('src', 'Assets/weather-icons/day.svg')
+        img_5.setAttribute('src', 'Assets/weather-icons/sun.png')
         img_5.setAttribute('alt', 'clear wheater')
-        
-      } else if (data.list[i*7].weather[0].main == "broken clouds"){
-        img_5.classList.remove('hide')
-        img_5.setAttribute('src', 'Assets/weather-icons/cloudy-day-1.svg')
-        img_5.setAttribute('alt', 'broken clouds')
       
       } else if (data.list[i*7].weather[0].main == "Clouds"){
         img_5.classList.remove('hide')
-        img_5.setAttribute('src', 'Assets/weather-icons/cloudy.svg')
+        img_5.setAttribute('src', 'Assets/weather-icons/cloudy.png')
         img_5.setAttribute('alt', 'clouds')
       
       } else if (data.list[i*7].weather[0].main == "Rain"){
         img_5.classList.remove('hide')
-        img_5.setAttribute('src', 'Assets/weather-icons/rainy-6.svg')
+        img_5.setAttribute('src', 'Assets/weather-icons/rain.png')
         img_5.setAttribute('alt', 'rain')
       
       } else if (data.list[i*7].weather[0].main == "Snow"){
         img_5.classList.remove('hide')
-        img_5.setAttribute('src', 'Assets/weather-icons/snowy-5.svg')
+        img_5.setAttribute('src', 'Assets/weather-icons/snowy.png')
         img_5.setAttribute('alt', 'snow')
       
       } else{
         img_5.classList.remove('hide')
-        img_5.setAttribute('src', 'Assets/weather-icons/thunder.svg')
+        img_5.setAttribute('src', 'Assets/weather-icons/extreme.png')
         img_5.setAttribute('alt', 'extreme')
       }
   }
 }
 
-// Calling the get_wheater function after click on the button
-button_city.addEventListener('click', get_city);
+// If one of the cities that already been chosen is clicked, then show the value of it again
+function get_city_chosen(e) {
 
-var button_get = document.querySelector('.get');
-button_get.addEventListener('click', get_city_chosen);
-
-function get_city_chosen() {
-  var button_cities_attribute = button_get.getAttribute('data-value')
+// Targeting the button that was clicked and getting it the attribute
+  var button_cities_attribute = e.target.getAttribute('data-value')
+  
+// City is equal the data-value that is on the button
   city = button_cities_attribute
+
+// Adding the var city to the API and getting the weather of the city
   const url_api_weather = ('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=6086d1f039acf014abeacd1138429b35&units=imperial&q='+city+',EUA')
+
   // Getting the data from the weather API using the input value
     fetch(url_api_weather)
       .then(function (response){
@@ -171,3 +191,6 @@ function get_city_chosen() {
         return get_wheater(data);
     })
 }
+
+// Calling the get_wheater function after click on the button
+button_city.addEventListener('click', get_city);
